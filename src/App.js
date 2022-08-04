ba6837a0fc68a33fe77da4689dc04f99c2571e38
Tyler Case <tcase629@yahoo.com>
2022-08-04 16:42:59 -0600

    add components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react'
 import './App.css'
+import Home from './components/Home'
+import Footer from './components/Footer'
 
 export default function App() {
   return (
-    <div className="main">BREW BUDDY</div>
+    <div className="main-container">
+      <Home />
+      <Footer />
+    </div>
   )
 }
---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div className="home-container">
+      <h1 className="title">Brew Buddy</h1>
+      <img alt="big 'ol beer" src="images/beermug.png" />
+    </div>
+  )
+}
+
+ 
