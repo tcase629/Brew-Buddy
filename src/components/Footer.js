49d4f6d10eaa93c38acc904c422325817a1d636b
Tyler Case <tcase629@yahoo.com>
2022-08-07 08:37:30 -0600

    setup footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,35 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <nav className="mobile-nav">
+      <ul>
+        <li>
+          <a href="/calc1" className="icon">
+            <img src="images/brew.png" alt="" />
+          </a>
+        </li>
+        <li>
+          <a href="/calc2" className="icon">
+            <img src="images/hops.png" alt="" />
+          </a>
+        </li>
+        <li>
+          <a href="/calc3" className="icon">
+            <img src="images/keg.png" alt="" />
+          </a>
+        </li>
+        <li>
+          <a href="/calc4" className="icon">
+            <img src="images/pot.png" alt="" />
+          </a>
+        </li>
+        <li>
+          <a href="/calc5" className="icon">
+            <img src="images/wheat.png" alt="" />
+          </a>
+        </li>
+      </ul>
+    </nav>
   )
 }
 
