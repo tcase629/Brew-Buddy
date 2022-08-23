631346c8d4c1528e379dd61a643d2ce554d01f15
Tyler Case <tcase629@yahoo.com>
2022-08-23 17:30:07 -0600

    strike water calculation

--- src/pages/StrikeWater.js
+++ src/pages/StrikeWater.js
@@ -2,9 +2,53 @@
 import React from 'react'
 
 export default function StrikeWater() {
+  const [strikeData, setStrikeData] = React.useState(
+    {targetTemp: "", grainTemp: "", ratio: ""}
+  )
+
+  function handleChange(event) {
+    setStrikeData(prevStrikeData => {
+      return {
+        ...prevStrikeData,
+        [event.target.name]: event.target.value
+      }
+    })
+  }
+
+  const tTemp = strikeData.targetTemp
+  const gTemp = strikeData.grainTemp
+  const ratio = strikeData.ratio
+  const first = (0.2 / ratio) * (tTemp - gTemp)
+  const strikeTotal = Math.round(parseInt(first) + parseInt(tTemp))
+  //fix calculation JS (0.2 / r) * (t2 - t1) + t2
+
   return (
     <div>
       <h1>Strike Water Calculator</h1>
+        <form>
+            <input 
+              type="text" 
+              placeholder="Target Temperature"
+              name="targetTemp"
+              onChange={handleChange}
+              value={strikeData.targetTemp}
+            />
+            <input 
+              type="text" 
+              placeholder="Grain Temperature"
+              name="grainTemp"
+              onChange={handleChange}
+              value={strikeData.grainTemp}
+            />
+            <input 
+              type="text" 
+              placeholder="Water to Grain Ratio (quarts per pound)"
+              name="ratio"
+              onChange={handleChange}
+              value={strikeData.ratio}
+            />
+            <div>{strikeTotal}° F</div>
+        </form>
     </div>
   )
 }
