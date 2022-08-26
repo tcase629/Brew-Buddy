32c2cb0ff49411dfe8bb0c5c9a7c095154e57e78
Tyler Case <tcase629@yahoo.com>
2022-08-26 17:13:22 -0600

    Gravity and Dilution components setup

--- src/pages/Gravity.js
+++ src/pages/Gravity.js
@@ -2,9 +2,40 @@
 import React from 'react'
 
 export default function Gravity() {
+
+function handleChange() {
+
+}
+
+const answer = "answer"
+
   return (
     <div>
       <h1>Gravity Adjustment Calculator</h1>
+        <form>
+          <input 
+            type="text" 
+            placeholder="Volume"
+            name=""
+            value=""  
+            onChange={handleChange}
+          />
+          <input 
+            type="text" 
+            placeholder="Meausured Gravity"
+            name=""
+            value="" 
+            onChange={handleChange} 
+          />
+          <input 
+            type="text" 
+            placeholder="Target Gravity"
+            name=""
+            value=""  
+            onChange={handleChange}
+          />
+        </form>
+        <div>{answer}</div>
     </div>
   )
 }
